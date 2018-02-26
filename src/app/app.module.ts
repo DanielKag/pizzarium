1c645e4941c039e082f03e1903f987e8baa8870d
Nadav Mary <danielkagan86@gmail.com>
2018-02-26 12:16:20 +0200

    Add redux to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-
+import { NgReduxModule, NgRedux, DevToolsExtension } from '@angular-redux/store'
+import { combineReducers } from 'redux';
 
 import { AppComponent } from './app.component';
 import { OrderDetailsComponent } from './containers/order-details/order-details.component';
@@ -11,11 +12,13 @@ import { TopComponent } from './components/top/top.component';
 import { SelectSizeComponent } from './components/select-size/select-size.component';
 import { SelectToppingsComponent } from './components/select-toppings/select-toppings.component';
 import { PizzaDetailsComponent } from './components/pizza-details/pizza-details.component';
+import { environment } from '../environments/environment'
+import { IDataFromServerState, dataFromServer } from './reducers/data-from-server.reducer';
 
 // PrimeNG
-import {ButtonModule} from 'primeng/button';
-import {SelectButtonModule} from 'primeng/selectbutton';
-import {ToggleButtonModule} from 'primeng/togglebutton';
+import { ButtonModule } from 'primeng/button';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { ToggleButtonModule } from 'primeng/togglebutton';
 import { ImageSelectorComponent } from './components/image-selector/image-selector.component';
 
 
@@ -25,6 +28,8 @@ const routes: Routes = [
   { path: 'cart', component: CartComponent },
 ];
 
+export interface IPizzariumState extends IDataFromServerState {
+}
 
 @NgModule({
   declarations: [
@@ -38,6 +43,7 @@ const routes: Routes = [
     ImageSelectorComponent
   ],
   imports: [
+    NgReduxModule,
     BrowserModule,
     FormsModule,
     RouterModule.forRoot(routes),
@@ -48,4 +54,20 @@ const routes: Routes = [
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  
+  constructor(ngRedux: NgRedux<any>,
+              devTools: DevToolsExtension) {
+    const reducers = combineReducers<any>(Object.assign({}, dataFromServer));
+    let enhancers = [];
+    const rootReducer = (state, action) => {
+      return reducers(state, action);
+    }
+
+    if (!environment.production && devTools.isEnabled()) {
+      enhancers = [...enhancers, devTools.enhancer()];
+    }
+
+    ngRedux.configureStore(rootReducer, undefined, undefined, enhancers);
+  }
+ }
