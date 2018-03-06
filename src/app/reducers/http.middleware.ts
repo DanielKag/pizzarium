d22c8385030cd5d5835fceb3c044c121485f7456
Daniel Kagan <you@example.com>
2018-03-06 16:49:01 +0200

    add function to middleware

--- src/app/reducers/http.middleware.ts
+++ src/app/reducers/http.middleware.ts
@@ -1,32 +1,34 @@
-
-export const HTTP_ACTION = 'HTTP_ACTION'
-
 export const httpMiddleware = store => next => action => {
     if(action.payload && action.payload.isHttp) {
-        next({
-            type: action.type + '_PENDING',
-            payload: action.payload
-        });
-
-        fetch(action.payload.url)
-            .then(response => response.json())
-            .then(response => {
-                next({
-                    type: action.type + '_SUCCESS',
-                    payload: {
-                        response
-                    }
-                });
-            })
-            .catch(error => {
-                next({
-                    type: action.type + '_FAILURE',
-                    payload:{
-                        error
-                    }
-                })
-            });
+        handleHTTPAction(next, action);
     } else {
         next(action);
     }
 };
+
+const handleHTTPAction = (next, action) => {
+    
+    next({
+            type: action.type + '_PENDING',
+            payload: action.payload
+        });
+
+    fetch(action.payload.url)
+        .then(response => response.json())
+        .then(response => {
+            next({
+                type: action.type + '_SUCCESS',
+                payload: {
+                    response
+                }
+            });
+        })
+        .catch(error => {
+            next({
+                type: action.type + '_FAILURE',
+                payload:{
+                    error
+                }
+            })
+        });
+}
